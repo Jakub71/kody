47c8fd950ac164eeda99118b4ab62307f2f2aadc
Jakub71 <Jakubjan0208@interia.pl>
2018-09-11 14:19:30 +0200

    elo

---
+++ 2 klasa/cpp/wejsciewyjscie.cpp
@@ -0,0 +1,48 @@
+/*
+ * komunikat.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a, b;
+    a = b = 0;
+    // DRY- Don't repeat yourself - naczelna zasada programisty
+    cout << "Podaj pierwszą liczbę:";
+    cin >> a;
+    cout << a;
+    
+    cout << endl << "Podaj drugą liczbę:"; // nowa linia endl lub "\n"
+    cin >> b;
+    cout<< b;
+	
+    cout << endl << "Suma: " << a + b;
+    cout << endl << "Różnica: " << a - b;
+    cout << endl << "Iloraz: " << a / b;
+    cout << endl << "Iloczyn: " << a * b;
+    return 0;
+}
+
