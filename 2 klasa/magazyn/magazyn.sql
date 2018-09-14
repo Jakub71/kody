e8924d4011690c879799b3a3f9bf6e05d216ca13
Jakub71 <Jakubjan0208@interia.pl>
2018-09-14 08:12:28 +0200

    elo2

--- 2 klasa/magazyn/magazyn.sql
+++ 2 klasa/magazyn/magazyn.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS dane_customers;
 CREATE TABLE dane_customers (
-   customer_id INTEGER PRIMARY KEY,
+    customer_id INTEGER PRIMARY KEY,
     customer_name TEXT NOT NULL,
-    adress TEXT DEFAULT 
+    adress TEXT
 );
 
 DROP TABLE IF EXISTS dane_orders;
 CREATE TABLE dane_orders (
     order_id INTEGER PRIMARY KEY,
-    custpmer_id INTEGER,
+    customer_id INTEGER,
     subscription_id INTEGER,
     purchase_date DATE,
     FOREIGN KEY (customer_id) REFERENCES dane_customers(customer_id)
@@ -20,5 +20,5 @@ CREATE TABLE dane_subscriptions (
     subscription_id INTEGER PRIMARY KEY,
     description TEXT NOT NULL,
     price_per_month INTEGER,
-    subscription_length INTEGER,
+    subscription_length INTEGER
 );
