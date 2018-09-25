a28c566429f6bcd960d9a494f8afbd32754637f4
Jakub71 <Jakubjan0208@interia.pl>
2018-09-25 14:19:12 +0200

    elo

---
+++ 2 klasa/python/petle-cw1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  petle-cw1.py
+#  
+#  Copyright 2018  <>
+#  
+
+def main(args):
+    a = int(input("Podaj pierwszą liczbę:"))
+    b = int(input("Podaj drugą liczbę:"))
+    
+    while a + b >= 75:
+         a = int(input("Podaj pierwszą liczbę:"))
+         b = int(input("Podaj drugą liczbę:"))
+         
+         if a + b > 75
+       b = int(input("Za duża liczba!"))
+      
+        
+    for liczba in range(a, b + 1):
+        #if liczba % 2 == 0:
+        if not liczba % 2:
+            print(liczba)
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
