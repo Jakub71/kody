9ae0e81d4ed4fc6039ab208e206def711044d4f8
Jakub71 <Jakubjan0208@interia.pl>
2018-10-03 11:24:24 +0200

    elo2

---
+++ 2 klasa/python/trojka6t.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  prostokat.py
+#  
+#  Copyright 2018  <>
+
+def main(args):
+    
+    a = int(input("Podaj długość boku a: "))
+    
+    b = int(input("Podaj długość boku b: "))
+    
+    znak = input("Podaj znak:")
+    
+    for i in range(a):
+        for j in range(b):
+            if j > i and j < b:
+                print("", end='')
+            else:
+                print(znak, end='') 
+        print()
+    
+    return 0
+    
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
