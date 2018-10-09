e5e22e4fdb4bfc2b460c6aae3d906343f79c122d
Jakub71 <Jakubjan0208@interia.pl>
2018-10-09 14:19:06 +0200

    elo

---
+++ 2 klasa/python/liczby-23.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  liczbu-23.py
+#
+def liczby2():
+    for i in range(10, 99):
+        if i% 11 != 0:
+            print(i)
+       
+def liczby3():
+    for i in range(1, a + 1):
+        for j in range(1, b + 1):
+            print("{:>5} ".format(i * j), end='')
+        print()
+        
+def main(args):
+
+    return 0
+    
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
