9ebc726175db7c55428b468165633112fe79ccbb
Jakub71 <Jakubjan0208@interia.pl>
2018-10-10 07:19:25 +0200

    elo5

--- 2 klasa/python/liczby-23.py
+++ 2 klasa/python/liczby-23.py
@@ -3,19 +3,20 @@
 #
 #  liczbu-23.py
 #
-def liczby2():
-    for i in range(10, 99):
-        if i% 11 != 0:
-            print(i)
-       
-def liczby3():
-    for i in range(1, a + 1):
-        for j in range(1, b + 1):
-            print("{:>5} ".format(i * j), end='')
+def liczby2(a = 10, b = 99):
+    for liczba in range(a, b):
+        if liczba % 11 != 0:
+            print(liczba)
+    print()   
+def liczby3(a = 100, b = 999):
+    for liczba in range(a, b):
+        if liczba % 11 != 0:
+            print(liczba)
         print()
         
 def main(args):
-
+    print(liczby2())
+    print(liczby3())
     return 0
     
 if __name__ == '__main__':
