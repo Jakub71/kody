4fb2362f15ec0138d789837bff61dc92f25bd007
Jakub71 <Jakubjan0208@interia.pl>
2018-10-23 16:37:23 +0200

    kurs

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  hello.py
+#  
+ 
+print("Hello")
+ 
+ 
+ 
+def witaj():
+    print("hello world!")
+    a = input("Podaj liczbe 1: ")
+    b = input("Podaj liczbe 2: ")
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(witaj())
---
+++ st.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+    # ustawienia do sublime
+// editor options
+    // "draw_white_space": "all",
+    // tabs and whitespace
+    "auto_indent": true,
+  "ensure_newline_at_eof_on_save": true,
+  "folder_exclude_patterns":
+  [
+    ".svn",
+    ".git",
+    ".hg",
+    "CVS",
+    "__pycache__"
+  ],
+  "indent_to_bracket": true,
+    "rulers": [79],
+    "shift_tab_unindent": true,
+    "smart_indent": true,
+    "tab_size": 4,
+    "translate_tabs_to_spaces": true,
+    "trim_automatic_white_space": true,
+    "use_tab_stops": true,
+    "word_wrap": true,
+    "wrap_width": 80
+}
