3c473b1af8153a7ee74006c2548e1907597866ea
Jakub71 <Jakubjan0208@interia.pl>
2018-10-31 11:05:16 +0100

    kwitek

--- 2 klasa/cpp/znak.cpp
+++ 2 klasa/cpp/znak.cpp
@@ -9,18 +9,49 @@ using namespace std;
 
 void licz_znaki(char tab[]){
     int i = 0;
-    int biale = 0;
+    int biale, inter, licz;
+    biale = inter = licz = 0;
     while(tab[i] != '\0') {
         cout << tab[i] << " ";
-        if (tab[i] == ' ' || tab[i] == '\t')
-            biale++;
-        else
-            cout << tab[i];
+       //~ if (tab[i] == ' ' || tab[i] == '\t')
+          //~  biale++;
+        //~else
+           //~ cout << tab[i];
+        switch (tab[i]) {
+            case ' ': biale++; break;
+            case '\t': biale++; break;
+            case ',': inter++; break;
+            case '.': inter++; break;
+            default: licz++; break;
+        }
         i++; 
     }
-    cout << "\nZnaków białych: " << biale << endl;    
+    cout << "\nZnaków białych: " << biale << endl; 
+    cout << "\nZnaków interpunkcyjnych: " << inter << endl;
+    cout << "\nReszta: " << licz << endl;   
 }
 
+void ascii(char tab[]) {
+    int i = 0;
+    while(tab[i] != '\0') {
+        cout << (int)tab[i] << " ";
+        i++;
+    }
+}
+// A-Z ASC2 65-90, a-z ASC2 97- 122 ( zakresy dużych i małych liter)
+void zamiana_liter(char tab[]){
+    int i = 0;
+    int z = 0;
+    while(tab[i] != '\0') {
+        z = (int)tab[i];
+        if (z >= 65 && z <= 90){
+         z = z + 32;   
+         }
+         cout << (char)z << " ";   
+        i++;
+    }
+    
+}
 int main(int argc, char **argv)
 {
     const int rozmiar = 30; // deklaracja stałej
@@ -28,8 +59,9 @@ int main(int argc, char **argv)
     cout << "Jak się nazywasz? ";
     cin.getline(znaki, rozmiar);
     cout << "Cześć " << znaki << endl;
-    licz_znaki(znaki);
-	
+    // licz_znaki(znaki);
+    ascii(znaki);
+	zamiana_liter(znaki);
 	return 0;
 }
 
