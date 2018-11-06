ad3318b4d682260a4cb7f764948f21e496b5f2c4
Jakub71 <Jakubjan0208@interia.pl>
2018-11-06 15:24:42 +0100

    elomichal

--- kurs/reszta.py
+++ kurs/reszta.py
@@ -3,6 +3,19 @@
 #
 #  reszta.py
 
+def pobierzNominaly():
+    nominaly = set([200, 100, 50, 20, 10, 5, 2, 1])
+    n = int(input('Podaj nominał lub 0 aby zakończyć: '))
+    listaNm = []
+    while n > 0:
+        if n in nominaly:
+            listaNm.append(n)
+        else:
+            print('Błąd')
+        n = int(input('Podaj nominał lub 0 aby zakończyć: '))
+    listaNm.sort(reverse=True) # sortowanie malejące listy
+    return listaNm
+    
 def wydajReszte1(r, l):
     i = 0
     while r > 0:
@@ -13,10 +26,34 @@ def wydajReszte1(r, l):
         
         i += 1
 
+def wydajReszte2(r, listaNm):
+    i = 0
+    liczbaNm = len(listaNm)
+    while r > 0 and i < liczbaNm:
+        while i < liczbaNm and r < listaNm[i]:
+            i += 1
+        if i < liczbaNm and r >= listaNm[i]:
+            nominal = listaNm[i]
+            ileNm = int(r / nominal)
+            if ileNm > listaNm.count(nominal):
+                ileNm = listaNm.count(nominal)
+            r -= ileNm * nominal
+            for j in range(ileNm):
+                listaNm.remove(nominal)
+                liczbaNm -= 1
+            i = 0
+            print("{} x {}".format(ileNm, nominal))
+    if r > 0:
+        print('Brak nominałów do wydania {} zł'.format(r))
+            
 def main(args):
-    listaNm = [200, 100, 50, 20, 10, 5, 2, 1]
+    #listaNm = [20, 10, 5, 2, 1]
+    listaNm = pobierzNominaly()
+    #print(listaNm)
+    #return
     reszta = int(input('Podaj resztę :'))
-    wydajReszte1(reszta, listaNm)
+    #wydajReszte1(reszta, listaNm)
+    wydajReszte2(reszta, listaNm)
     return 0
 
 if __name__ == '__main__':
