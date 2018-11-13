fd737beaab35bf2dd01ccd64531044af8f382111
Jakub71 <Jakubjan0208@interia.pl>
2018-11-13 16:18:52 +0100

    bijatyka

---
+++ kurs/stos_str2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  stos_str2.py
+
+stos = [] # pusta lista, zasięg globalny
+SP = 0 # wskaźnik wierzchołka
+
+def push(stos, rozmiar, SP, dane):
+    if SP < rozmiar:
+        stos[SP] = dane
+        SP += 1 # inkrementacja (zwiekszenie o 1)
+    else:
+        print("Stack overflow!")
+    return SP
+    
+def pop(stos, rozmiar, SP):
+    element = None
+    SP -= 1 # dekrementacja
+    if SP > -1:
+        element = stos[SP]
+        stos[SP] = None
+    else:
+        print("Stack overflow!")
+        
+    return SP, element
+        
+def main(args):
+    stos = [] # pusta lista, zasięg globalny
+    SP = 0 # wskaźnik wierzchołka
+    rozmiar = 3
+    stos = [None] * rozmiar
+    SP = push(stos, rozmiar,SP, 2)
+    SP = push(stos, rozmiar,SP, 5)
+    SP, element = pop(stos, rozmiar, SP)
+    print(element)
+    SP = push(stos, rozmiar,SP, 3)
+    SP, element = pop(stos, rozmiar, SP)
+    print(element)
+    SP, element = pop(stos, rozmiar, SP)
+    print(element)
+    SP, element = pop(stos, rozmiar, SP)
+    print(element)
+    print(SP)
+    print(stos)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
