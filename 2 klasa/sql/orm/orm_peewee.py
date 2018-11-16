e5edb4b21fd93260cbebd648f942e8784bdcaf5e
Jakub71 <Jakubjan0208@interia.pl>
2018-11-16 08:45:29 +0100

    elo

--- 2 klasa/sql/orm/orm_peewee.py
+++ 2 klasa/sql/orm/orm_peewee.py
@@ -33,10 +33,29 @@ class Wynik(BazaModel):
     uczen = ForeignKeyField(Uczen, related_name='wyniki')
     
 def main(args):
-    if os.path_exists(baza_plik):
+    if os.path.exists(baza_plik):
         os.remove(baza_plik)
     baza.connect() # połączenie z bazą
     baza.create_tables([Klasa, Uczen, Wynik])
+    
+    kl2a = Klasa(nazwa="2A", roknaboru=2010, rokmatury=2013)
+    kl2a.save()
+    
+    u1 = Uczen(imie="Jarosław", nazwisko="Kaczyński", plec=False, klasa=kl2a)
+    u1.save()
+    
+    kl1a = Klasa(nazwa="1A", roknaboru=2009, rokmatury=2012)
+    kl1a.save()
+    
+    u2 = Uczen(imie="Anna", nazwisko="Gacek", plec=True, klasa=kl1a)
+    u2.save()
+    u3 = Uczen(imie="Roman", nazwisko="Polek", plec=False, klasa=kl1a)
+    u3.save()
+    
+    uczniowie = Uczen.select()
+    for uczen in uczniowie:
+        print (uczen.id, uczen.nazwisko, uczen.klasa.nazwa)
+    
     return 0
 
 if __name__ == '__main__':
