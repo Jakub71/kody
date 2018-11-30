eff26d011338a6ba374d27513324f3ed6cbd4290
Jakub71 <Jakubjan0208@interia.pl>
2018-11-30 07:35:29 +0100

    elo

--- 2 klasa/sql/uczniowie/Drypa_orm_uczniowie.py
+++ 2 klasa/sql/uczniowie/Drypa_orm_uczniowie.py
@@ -18,38 +18,38 @@ class BazaModel(Model):
 class Klasa(BazaModel):
     
     nazwa = CharField(null=False)
-    roknaboru = IntegerField(default=0)
-    rokmatury = IntegerField(default=0)
+    rok_naboru = IntegerField(default=0)
+    rok_matury = IntegerField(default=0)
 
 class Uczen(BazaModel):
     
     imie = CharField(null=False)
     nazwisko = CharField(null=False)
-    plec = BooleanField()
-    idklasa = ForeignKeyField(Klasa, related_name='uczniowie')
-    egzhum = FloatField(default = 0)
-    egzmat = FloatField(default = 0)
-    egzjez = FloatField(default = 0)
+    plec = IntegerField()
+    klasa = ForeignKeyField(Klasa, related_name='uczniowie')
+    egz_hum = FloatField(default = 0)
+    egz_mat = FloatField(default = 0)
+    egz_jez = FloatField(default = 0)
     
 class Przedmiot(BazaModel):
    
     przedmiot = CharField(null=False)
-    imienaucz = CharField(null=False)
-    nazwiskonaucz = CharField(null=False)
-    plecnaucz = BooleanField()
+    imie_naucz = CharField(null=False)
+    nazwisko_naucz = CharField(null=False)
+    plec_naucz = IntegerField()
     
-class Oceny(BazaModel):
+class Ocena(BazaModel):
     
     datad = DateField()
-    iduczen = ForeignKeyField(Uczen, related_name='oceny')
-    idprzedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
-    ocena = DecimalField(null=False)
+    uczen = ForeignKeyField(Uczen, related_name='oceny')
+    przedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
+    ocena = FloatField(null=False)
     
 def main(args):
     if os.path.exists(baza_plik):
         os.remove(baza_plik)
     baza.connect() # połączenie z bazą
-    baza.create_tables([Klasa, Uczen, Przedmiot, Oceny])
+    baza.create_tables([Klasa, Uczen, Przedmiot, Ocena])
     return 0
 
 if __name__ == '__main__':
