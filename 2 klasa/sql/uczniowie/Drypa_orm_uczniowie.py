ccfe52a811c2ee62e22b46c024a0cd77365b1f4b
Jakub71 <Jakubjan0208@interia.pl>
2018-11-30 08:24:05 +0100

    hghghnfhj

--- 2 klasa/sql/uczniowie/Drypa_orm_uczniowie.py
+++ 2 klasa/sql/uczniowie/Drypa_orm_uczniowie.py
@@ -2,54 +2,36 @@
 # -*- coding: utf-8 -*-
 #
 #  orm_peewee.py 
-
 import os
-from peewee import *
+from modele import *
+from baza import dane_z_pliku
 
-baza_plik = 'test.db'
-baza =SqliteDatabase(baza_plik) #instancja bazy
-
-
-### MODELE DANYCH
-class BazaModel(Model):
-    class Meta:
-        database = baza
+def dodaj_dane(dane):
+    
+    for model, plik in dane.items():
+        pola = [pole for pole in model._meta.fields]
+        pola.pop(0) # usuwanie pierwszego rekordu z listy
         
-class Klasa(BazaModel):
-    
-    nazwa = CharField(null=False)
-    rok_naboru = IntegerField(default=0)
-    rok_matury = IntegerField(default=0)
+        wpisy = dane_z_pliku(plik + '.csv')
+        print(wpisy)
+        #model.insert_many(wpisy, fields=)
 
-class Uczen(BazaModel):
-    
-    imie = CharField(null=False)
-    nazwisko = CharField(null=False)
-    plec = IntegerField()
-    klasa = ForeignKeyField(Klasa, related_name='uczniowie')
-    egz_hum = FloatField(default = 0)
-    egz_mat = FloatField(default = 0)
-    egz_jez = FloatField(default = 0)
-    
-class Przedmiot(BazaModel):
-   
-    przedmiot = CharField(null=False)
-    imie_naucz = CharField(null=False)
-    nazwisko_naucz = CharField(null=False)
-    plec_naucz = IntegerField()
-    
-class Ocena(BazaModel):
-    
-    datad = DateField()
-    uczen = ForeignKeyField(Uczen, related_name='oceny')
-    przedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
-    ocena = FloatField(null=False)
-    
+
 def main(args):
     if os.path.exists(baza_plik):
         os.remove(baza_plik)
     baza.connect() # połączenie z bazą
     baza.create_tables([Klasa, Uczen, Przedmiot, Ocena])
+    
+    dane = {
+        Klasa: 'klasy',
+        Uczen: 'uczniowie',
+        Przedmiot: 'przedmioty',
+        Ocena: 'oceny',
+    }
+    dodaj_dane(dane)
+    
+    baza.close()
     return 0
 
 if __name__ == '__main__':
