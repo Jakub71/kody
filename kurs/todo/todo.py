311fd69bd07dc69c1acd5fa03cc95be796e4945e
Jakub71 <Jakubjan0208@interia.pl>
2018-12-04 16:39:14 +0100

    pong

--- kurs/todo/todo.py
+++ kurs/todo/todo.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-#  todo.py
 
 from PyQt5.QtWidgets import QApplication, QWidget
-from gui import Ui_Widget
+from gui import Ui_Widget, LoginDialog
 from PyQt5.QtWidgets import QMessageBox, QInputDialog
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QDialogButtonBox
-from PyQt5.QtWidgets import QLabel, QLineEdit
-from PyQt5.QtWidgets import QGridLayout
+import baza
+from tbmodel import TbModel, pola
 
 class Zadania(QWidget, Ui_Widget):
 
@@ -19,73 +15,71 @@ class Zadania(QWidget, Ui_Widget):
         
         self.logujBtn.clicked.connect(self.loguj)
         self.koniecBtn.clicked.connect(self.koniec)
-        
+        self.dodajBtn.clicked.connect(self.dodaj)
+        self.zapiszBtn.clicked.connect(self.zapisz)
+
+    def zapisz(self):
+        baza.zapiszDane(baza.sesja, model.tabela)
+        model.layoutChanged.emit()
+
+
+    def dodaj(self):
+        """ Dodawanie nowego zadania """
+        zadanie, ok = QInputDialog.getMultiLineText(self,
+                                                    'Zadanie',
+                                                    'Co jest do zrobienia?')
+        if not ok or not zadanie.strip():
+            QMessageBox.critical(self,
+                                 'Błąd',
+                                 'Zadanie nie może być puste.',
+                                 QMessageBox.Ok)
+            return
+
+        zadanie = baza.dodajZadanie(baza.sesja, self.osoba, zadanie)
+        model.tabela.append(zadanie)
+        model.layoutChanged.emit()  # wyemituj sygnał: zaszła zmiana!
+        if len(model.tabela) == 1:  # jeżeli to pierwsze zadanie
+            self.odswiezWidok()     # trzeba przekazać model do widoku
+    def koniec(self):
+        self.close()
+
     def loguj(self):
         login, haslo, ok = LoginDialog.getLoginHaslo(self)
         if not ok:
             return
-
+            
         if not login or not haslo:
-            QMessageBox.warning(self, 'Błąd',
-                                'Pusty login lub hasło!', QMessageBox.Ok)
+            QMessageBox.warning(
+                                self, 'Błąd', 'Pusty login lub hasło!',
+                                QMessageBox.Ok)
+            return
+            
+        self.osoba = baza.loguj(baza.sesja, login, haslo)
+        if self.osoba is None:
+            QMessageBox.critical(self, 'Błąd', 'Błędne dane!')
             return
 
-        QMessageBox.information(self, 'Dane logowania', 'Podano: ' +
-                                login + ' ' + haslo, QMessageBox.Ok)
-        
-    def koniec(self):
-        self.close()
-    
-class LoginDialog(QDialog):
-    """ Okno dialogowe logowania """
+        # ~QMessageBox.information(
+                    # ~self, 'Dane logowania',
+                    # ~'Podano: ' + login + ' ' + haslo, QMessageBox.Ok)
+        zadania = baza.pobierzDane(baza.sesja, self.osoba)
+        model.aktualizuj(zadania)
+        model.layoutChanged.emit()
+        self.odswiezWidok()
+        self.dodajBtn.setEnabled(True)
+        self.zapiszBtn.setEnabled(True)
 
-    def __init__(self, parent=None):
-        super(LoginDialog, self).__init__(parent)
-
-        # etykiety, pola edycyjne i przyciski ###
-        loginLbl = QLabel('Login')
-        hasloLbl = QLabel('Hasło')
-        self.login = QLineEdit()
-        self.haslo = QLineEdit()
-        self.haslo.setEchoMode(QLineEdit.Password)
-        self.przyciski = QDialogButtonBox(
-            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
-            Qt.Horizontal, self)
-
-        # układ główny ###
-        uklad = QGridLayout(self)
-        uklad.addWidget(loginLbl, 0, 0)
-        uklad.addWidget(self.login, 1, 0)
-        uklad.addWidget(hasloLbl, 2, 0)
-        uklad.addWidget(self.haslo, 3, 0)
-        uklad.addWidget(self.przyciski, 4, 0, 4, 0)
-
-        # sygnały i sloty ###
-        self.przyciski.accepted.connect(self.accept)
-        self.przyciski.rejected.connect(self.reject)
-
-        # właściwości widżetu ###
-        self.setModal(True)
-        self.setWindowTitle('Logowanie')
-
-    def loginHaslo(self):
-        return (self.login.text().strip(),
-                self.haslo.text().strip())
-
-    # metoda statyczna, tworzy dialog i zwraca (login, haslo, ok)
-    @staticmethod
-    def getLoginHaslo(parent=None):
-        dialog = LoginDialog(parent)
-        dialog.login.setFocus()
-        ok = dialog.exec_()
-        login, haslo = dialog.loginHaslo()
-        return (login, haslo, ok == QDialog.Accepted)
-
+    def odswiezWidok(self):
+        self.widok.setModel(model)
+        self.widok.hideColumn(0)  #
+        self.widok.horizontalHeader().setStretchLastSection(True)
+        self.widok.resizeColumnsToContents()
 
 if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
+    model = TbModel(pola)
     okno = Zadania()
     okno.show()
     okno.move(350, 200)
