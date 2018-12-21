3e77ff8038c71d8aa6d23477281de0ca40b0eea6
Jakub71 <Jakubjan0208@interia.pl>
2018-12-21 07:34:11 +0100

    elo

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -22,10 +22,15 @@ def after_request(response):
 @app.route("/")
 def hello():
     return "<h1>Witaj świecie!</h1><h2>Aplikacja Quiz</h2>"
-
+    
+@app.route("/lista")
+def lista():
+    pytania = Pytanie.select()
+    return render_template('lista.html', query = pytania)
+    
 @app.route("/quiz")
 def quiz():
-    pytania = Pytanie.select()
+    pytania = Pytanie.select().join(Odpowiedz).distinct()
     return render_template('quiz.html', query = pytania)
     
 @app.route("/klasa")
