a6f97572b5a1d12e2bac4a91cbedc09adf10874b
Jakub71 <Jakubjan0208@interia.pl>
2018-12-21 07:55:16 +0100

    elo43

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -3,11 +3,10 @@
 #
 #  quiz.py
 #  
-from flask import Flask, g
-from flask import render_template, request
+from flask import g
 from modele import *
+from views import *
 
-app = Flask(__name__)
 
 @app.before_request
 def before_request():
@@ -18,24 +17,6 @@ def before_request():
 def after_request(response):
     g.db.close()
     return response
-    
-@app.route("/")
-def index():
-    return render_template('index.html')
-    
-@app.route("/lista")
-def lista():
-    pytania = Pytanie.select()
-    return render_template('lista.html', query = pytania)
-    
-@app.route("/quiz")
-def quiz():
-    pytania = Pytanie.select().join(Odpowiedz).distinct()
-    return render_template('quiz.html', query = pytania)
-    
-@app.route("/klasa")
-def klasa():
-    return render_template('klasa.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
