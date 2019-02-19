3624617ff93a65c1693b29f19a5c2703ebd51266
Jakub71 <Jakubjan0208@interia.pl>
2019-02-19 18:30:30 +0100

    praca domowa

--- 2 klasa/cpp/szyfr_cezara.cpp
+++ 2 klasa/cpp/szyfr_cezara.cpp
@@ -1,35 +1,43 @@
 /*
  * szyfr_cezara.cpp
  */
-
-
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
 #define MAKS 100
 
 void szyfruj(char tekst[], int klucz){
     klucz = klucz % 26;
-    int i = 0;
     int kod = 0;
-    while(tekst[i] != '\0'){
-        kod = (int)tekst[i] + klucz;
-        cout << (char)kod;
-        i++;
-    } 
+    int ilosc = strlen(tekst);
     
+    for(int i = 0; i < ilosc; i++){
+        kod = (int)tekst[i] + klucz;
+        if (tekst[i] == ' ')
+            kod -= klucz;
+        else if(kod > 122)
+            kod -= 26;
+        tekst[i] = char(kod);
+        cout << tekst[i];
+    }
+
 }
+
 
 int main(int argc, char **argv)
 {
-	char tekst[100];
-    int klucz = 0;
+    char tekst[MAKS];
+	int klucz = 0;
     
-    cout << "Podaj tekst:\n ";
+    cout << "Podaj tekst: " << endl;
     cin.getline(tekst, MAKS);
-    cout << "Klucz? ";
+    
+    cout << "Podaj klucz: " << endl;
     cin >> klucz;
+    
     szyfruj(tekst, klucz);
 	return 0;
 }
-
