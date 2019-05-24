372423642b1407e0b8572e14a9c3041c302790c6
Jakub71 <Jakubjan0208@interia.pl>
2019-05-24 07:47:36 +0200

    elo123

--- template.php
+++ template.php
@@ -1,34 +1,17 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } ?>
-<?php include("header.inc.php"); ?>
-<body id="<?php get_page_slug(); ?>">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-
-<?php include("nav.inc.php"); ?>
-
-
-<main role="main" class="container">
-  <div class="row">
-  <div class="col-sm-8">
-    <h1><?php get_page_title(); ?></h1>
-    <?php get_page_content(); ?>
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mx-auto">
+      <?php get_navigation(return_page_slug()); ?>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Szukaj" aria-label="Szukaj">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Szukaj</button>
+    </form>
   </div>
-  <div class="col-sm-4">
-  <?php get_component('sidebar'); ?>
-  </div>
-</div>
-
-</main><!-- /.container -->
-
-<div class="container">
-<div class="row">
-  <div class="col">
-    <?php get_footer(); ?>
-  </div>
- </div>
-</div>
-<script src="<?php get_theme_url(); ?>/js/jquery.min.js"></script>
-<script src="<?php get_theme_url(); ?>/js/popper.min.js"></script>
-<script src="<?php get_theme_url(); ?>/js/bootstrap.min.js"></script>
-
-</body>
-</html>
+</nav>
